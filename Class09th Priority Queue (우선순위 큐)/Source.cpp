f7093a2d4eb04a647a0a23eecd2b0fb1599809c7
gydud0819 <pstar0819@naver.com>
2025-07-24 15:46:39 +0900

    (2025-07-24) 해시테이블

---
+++ Class09th Priority Queue (우선순위 큐)/Source.cpp
@@ -0,0 +1,277 @@
+﻿#include "Util.h"
+
+template <typename T>
+// 우선순위의 기준이... ㅇㅅㅇ
+class PriorityQueue		// 동적 배열로 만들기			// 정적이 아니라? 홀리몰리 
+{
+private:
+	int index;			// 배열 크기? 
+	int capacity;		// 메모리 공간 크기? 맞나? 아닌거 같은데 
+
+	T* container;
+public:
+	PriorityQueue() : index(0), capacity(0), container(nullptr) {}
+	~PriorityQueue()
+	{
+		// 소멸자 
+		if (container != nullptr)
+		{
+			delete[] container;
+		}
+	}
+public:
+	void resize(int newSize)
+	{
+		//capacity = newSize;		// 이게 아닌거 같은데 
+
+		// 2. 새로운 포인터 변수를 생성하고 새롭게 만들어진 메모리 공간을 가리키도록 하기
+		T* newContainer = new T[newSize];
+
+		// 3. 새로운 메모리 공간의 값 초기화하기
+		for (int i = 0; i < capacity; i++)
+		{
+			newContainer[i] = T();	// NULL도 가능
+
+		}
+
+		// 4. 기존 배열에 있는 값을 복사해서 새로운 배열에 넣어주기 
+		for (int i = 0; i < index; i++)
+		{
+			newContainer[i] = container[i];
+		}
+
+		// 5. 기존 배열의 메모리 해제하기
+		if (container != nullptr)
+			delete[] container;
+
+		// 6. 기존 배열을 가리키던 포인터 변수의 값을 새로운 배열의 시작 주소로 가리키기 
+		container = newContainer;
+		capacity = newSize;		// capacity를 새롭게 바꿔주기
+	}
+
+	// 우와............................... 이게 무슨 말이야 
+	void push(T data)
+	{
+		// 진짜 필요없나			// 필요하네 
+		if (capacity <= 0)
+		{
+			resize(1);
+		}
+
+		if (index >= capacity)
+		{
+			resize(capacity * 2);	// 2배 증가요? 
+		}
+
+		container[index++] = data;
+
+		int child = index - 1;		// 자식
+		int parent = (child - 1) / 2;		// 부모
+
+		// while 쓰는건가? 
+		while (child > 0)
+		{
+			//heapify 공식 들어가는 거 같고		// 비교해서 크면 바꾸고 아니면 냅둬야하는거 같은데 
+			if (container[child] > container[parent])
+			{
+				swap(container[child], container[parent]);
+			}
+
+			child = parent;
+			parent = (child - 1) / 2;
+		}
+
+		// child 공식 : child가 [1]이 될수 있게 공식 만들기 
+		// parent 공식 : (child - 1) / 2 = [0]
+		// swap을 이용해서 바꿔라(?) 
+	}
+
+	const T& top()
+	{
+		return container[0];
+	}
+
+	const bool& isEmpty()
+	{
+		return index <= 0;
+	}
+
+	void pop()		// 집가서 수정하기 
+	{
+		if (index <= 0)
+		{
+
+			cout << "priority queue is empty" << endl;
+		}
+
+		else
+		{
+			container[0] = container[--index];
+
+			int parent = 0;
+			while ((parent * 2 + 1) < index)
+			{
+				int child = parent * 2 + 1;
+
+				if (container[child] < container[child + 1])
+				{
+					child++;
+				}
+
+				if (container[child] < container[parent])
+				{
+					break;
+				}
+				else
+				{
+					swap(container[parent], container[child]);
+					parent = child;
+				}
+			}
+		}
+
+		//	if (index <= 0)
+		//	{
+		//		return;
+		//	}
+
+		//	 //전위? --index; <-이거? 
+		//	 //
+		//	 //왼쪽 : parent * 2 + 1
+		//	 //오른쪽 : 왼쪽 + 1
+
+		//	int parent = 0;
+
+		//	container[0] = container[--index];		// 이게 빠졌구나 
+
+		//	while ((parent * 2 + 1) < index)
+		//	{
+		//		int leftchild = parent * 2 + 1;		// 왼쪽 오른쪽 자식 변수 있어야하나용?
+		//		int rightchild = leftchild + 1;
+		//		int child = parent;			// 자식 변수 초기화? 맞나 
+
+		//		// 왼쪽 자식이 존재하고 왼쪽 자식이 부모보다 크면 왼쪽 자식으로 바꿔주기
+		//		if (leftchild < index && container[leftchild] > container[child])
+		//		{
+		//			child = leftchild;		// 왼쪽 자식이 더 크면 왼쪽 자식으로 바꿔주기
+		//		}
+
+		//		// 오른쪽 자식이 존재하고 오른쪽 자식이 부모보다 크면 
+		//		if (rightchild < index && container[rightchild] > container[child])
+		//		{
+		//			child = rightchild;		// 오른쪽 자식으로 바꿔주기
+		//		}
+
+		//		if (child == parent) { break; /*return;*/ }	// 자식이 안바뀌면 종료하기		// 리턴쓰는건 안되나 
+
+		//		swap(container[child], container[parent]);
+
+		//		parent = child;		// parent = child, child = parent 차이가 큰가? 
+		//	}
+
+	}
+};
+//#define SIZE 100
+//
+//class PriorityQueue {
+//private:
+//	int data[SIZE];			// 실제 데이터 저장
+//	int priority[SIZE];		// 우선순위 저장
+//	int count;				// 현재 큐에 있는 요소 수
+//
+//public:
+//	PriorityQueue() : count(0) {}
+//
+//	// 큐가 비었는지 확인
+//	bool isEmpty() {
+//		return count == 0;
+//	}
+//
+//	// 큐에 있는 요소 개수
+//	int size() {
+//		return count;
+//	}
+//
+//	// 우선순위를 고려해 정렬된 위치에 삽입
+//	void push(int value, int prio) {
+//		if (count >= SIZE) {
+//			cout << "Queue is Full" << endl;
+//			return;
+//		}
+//
+//		int i = count - 1;
+//
+//		// 우선순위 높은 순으로 정렬 (큰 숫자가 높은 우선순위라고 가정)
+//		while (i >= 0 && priority[i] < prio) {
+//			data[i + 1] = data[i];
+//			priority[i + 1] = priority[i];
+//			i--;
+//		}
+//
+//		// 새 값 삽입
+//		data[i + 1] = value;
+//		priority[i + 1] = prio;
+//		count++;
+//	}
+//
+//	// 가장 높은 우선순위 값을 꺼냄
+//	int pop() {
+//		if (isEmpty()) {
+//			cout << "Queue is Empty" << endl;
+//			return -1;
+//		}
+//
+//		int result = data[0];
+//
+//		// 하나씩 앞으로 당기기
+//		for (int i = 1; i < count; i++) {
+//			data[i - 1] = data[i];
+//			priority[i - 1] = priority[i];
+//		}
+//
+//		count--;
+//		return result;
+//	}
+//
+//	// 디버깅용: 큐 안에 뭐 들었는지 보기
+//	void print() {
+//		cout << "Queue: ";
+//		for (int i = 0; i < count; i++) {
+//			cout << "[" << data[i] << ", p=" << priority[i] << "] ";
+//		}
+//		cout << endl;
+//	}
+//};
+
+
+
+
+
+int main()
+{
+	PriorityQueue<int> pq;
+
+	pq.push(5);
+	pq.push(3);
+	pq.push(6);
+	pq.push(7);
+	pq.push(1);
+	pq.push(9);
+
+
+	//cout << pq.top() << endl;
+	//pq.pop();
+	//cout << pq.top() << endl;
+	//cout << pq.isEmpty() << endl;
+
+	while (pq.isEmpty() == false)
+	{
+		cout << pq.top() << endl;
+		pq.pop();
+	}
+
+
+	return 0;
+}
+
+
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,215 +1,113 @@
 ﻿#include "Util.h"
+#define SIZE 6
 
-template <typename T>
-// 우선순위의 기준이... ㅇㅅㅇ
-class PriorityQueue		// 동적 배열로 만들기			// 정적이 아니라? 홀리몰리 
+// 내일 eraser 예정 
+// 정적 배열로 설계 동적으로 공부햇는데 홀리몰리 
+template <typename K, typename V>		// K = Key, V = Value
+class HashTable
 {
-private:
-	int index;			// 배열 크기? 
-	int capacity;		// 메모리 공간 크기? 맞나? 아닌거 같은데 
+	// 또결리스트 ㅇㅅㅇ (단일 연결)
+	struct Node
+	{
+		K key;
+		V value;
+		Node* next;
 
-	T* container;
+	};
+
+	// 구조체 배열 만들기
+	struct Bucket
+	{
+		int count;
+		Node* head;
+	};
+	Bucket bucket[SIZE];
+
+
 public:
-	PriorityQueue() : index(0), capacity(0), container(nullptr) {}
-	~PriorityQueue()
+	HashTable()
 	{
-		// 소멸자 
-		if (container != nullptr)
+		for (int i = 0; i < SIZE; i++)
 		{
-			delete[] container;
+			bucket[i].count = 0;
+			bucket[i].head = nullptr;
 		}
 	}
-public:
-	void resize(int newSize)
+
+	// 소멸자 
+	~HashTable()
 	{
-		//capacity = newSize;		// 이게 아닌거 같은데 
+		Node* deleteNode;
+		Node* nextNode;
 
-		// 2. 새로운 포인터 변수를 생성하고 새롭게 만들어진 메모리 공간을 가리키도록 하기
-		T* newContainer = new T[newSize];
+		// bucket 비우기 
+		// 운동가지 마까... 또 불안해지네 ㅇㅂㅇ 조바심 내면 안되는데 
 
-		// 3. 새로운 메모리 공간의 값 초기화하기
-		for (int i = 0; i < capacity; i++)
+		for (int i = 0; i < SIZE; i++)
 		{
-			newContainer[i] = T();	// NULL도 가능
-
+			
 		}
-
-		// 4. 기존 배열에 있는 값을 복사해서 새로운 배열에 넣어주기 
-		for (int i = 0; i < index; i++)
-		{
-			newContainer[i] = container[i];
-		}
-
-		// 5. 기존 배열의 메모리 해제하기
-		if (container != nullptr)
-			delete[] container;
-
-		// 6. 기존 배열을 가리키던 포인터 변수의 값을 새로운 배열의 시작 주소로 가리키기 
-		container = newContainer;
-		capacity = newSize;		// capacity를 새롭게 바꿔주기
-	}
-
-	// 우와............................... 이게 무슨 말이야 
-	void push(T data)
-	{
-		// 진짜 필요없나			// 필요하네 
-		if (capacity <= 0)
-		{
-			resize(1);
-		}
-
-		if (index >= capacity)
-		{
-			resize(capacity * 2);	// 2배 증가요? 
-		}
-
-		container[index++] = data;
-
-		int child = index - 1;		// 자식
-		int parent = (child - 1) / 2;		// 부모
-
-		// while 쓰는건가? 
-		while (child > 0)
-		{
-			//heapify 공식 들어가는 거 같고		// 비교해서 크면 바꾸고 아니면 냅둬야하는거 같은데 
-			if (container[child] > container[parent])
-			{
-				swap(container[child], container[parent]);
-			}
-
-			child = parent;
-			parent = (child - 1) / 2;
-		}
-
-		// child 공식 : child가 [1]이 될수 있게 공식 만들기 
-		// parent 공식 : (child - 1) / 2 = [0]
-		// swap을 이용해서 바꿔라(?) 
-	}
-
-	const T& top()
-	{
-		return container[0];
-	}
-
-	const bool& isEmpty()
-	{
-		return index <= 0;
-	}
-
-	void pop()		// 집가서 수정하기 
-	{
-		if (index <= 0)
-		{
-			return;		// 비어있으면 그냥 리턴하기
-		}
-		// 전위? --index; <-이거? 
-
-		// 왼쪽 : parent * 2 + 1
-		// 오른쪽 : 왼쪽 + 1
-
-		int parent = 0;
-
-		while ((parent * 2 + 1) < index)
-		{
-		int leftchild = parent * 2 + 1;		// 왼쪽 오른쪽 자식 변수 있어야하나용?
-		int rightchild = leftchild + 1;
-		int child = parent;			// 자식 변수 초기화? 맞나 
 		
-			// 왼쪽 자식이 존재하고 왼쪽 자식이 부모보다 크면 왼쪽 자식으로 바꿔주기
-			if (leftchild < index && container[leftchild] > container[child])
-			{
-				child = leftchild;		// 왼쪽 자식이 더 크면 왼쪽 자식으로 바꿔주기
-			}
+		
+		if (bucket[head] != nullptr)
+		{
 
-			// 오른쪽 자식이 존재하고 오른쪽 자식이 부모보다 크면 
-			if (rightchild < index && container[rightchild] > container[child])
-			{
-				child = rightchild;		// 오른쪽 자식이 더 크면 오른쪽 자식으로 바꿔주기
-			}
-
-			if (child == parent) { break; }	// 자식이 안바뀌면 종료하기
-				
-			swap(container[child], container[parent]);
-
-			parent = child;		// parent = child, child = parent 차이가 큰가? 
 		}
-	
+		
+		deleteNode->next = nullptr;
+	}
+
+public:
+	template <typename T>
+	// 해쉬값 
+	const unsigned int& hash_function(T key)
+	{
+		// 양수만 저장할 수 있는 자료형? = key 값이 0 ~ 5까지 나올수있도록 하고 return 하기 나눠야하나 
+		return  (unsigned)key % SIZE;
+	}
+
+	Node* create_node(K key, V value)
+	{
+		Node* newNode = new Node;
+		// newNode에 key, value 넣기? 
+		newNode->key = key;			// 이건 또 뭐야 
+		newNode->value = value;
+		newNode = nullptr;
+
+		// 음 하라는 대로 했는데 이게 머지 
+
+		return newNode;				// 이게뭐야 
+	}
+
+	void insert(K key, V value)
+	{
+		// 1. 해시함수를 통해서 값을 받는 임시 변수 만들기 
+		int hashindex = hash_function(key);		// ? 
+		// 2. 새로운 노드 생성하기 
+		Node* newNode = create_node(key, value);		// ? 이게 뭐야 
+
+		// 노드가 1개라도 존재하지 않는다면
+		if (bucket[hashindex].count == 0)
+		{
+			bucket[hashindex].head = newNode;
+
+		}
+		else
+		{
+			newNode->next = bucket[hashindex].head;
+			bucket[hashindex].head = newNode;
+		}
+
+
+		bucket[hashindex].count++;
+
+
+
+		// 참조하라구요? 
+
 	}
 };
-//#define SIZE 100
-//
-//class PriorityQueue {
-//private:
-//	int data[SIZE];			// 실제 데이터 저장
-//	int priority[SIZE];		// 우선순위 저장
-//	int count;				// 현재 큐에 있는 요소 수
-//
-//public:
-//	PriorityQueue() : count(0) {}
-//
-//	// 큐가 비었는지 확인
-//	bool isEmpty() {
-//		return count == 0;
-//	}
-//
-//	// 큐에 있는 요소 개수
-//	int size() {
-//		return count;
-//	}
-//
-//	// 우선순위를 고려해 정렬된 위치에 삽입
-//	void push(int value, int prio) {
-//		if (count >= SIZE) {
-//			cout << "Queue is Full" << endl;
-//			return;
-//		}
-//
-//		int i = count - 1;
-//
-//		// 우선순위 높은 순으로 정렬 (큰 숫자가 높은 우선순위라고 가정)
-//		while (i >= 0 && priority[i] < prio) {
-//			data[i + 1] = data[i];
-//			priority[i + 1] = priority[i];
-//			i--;
-//		}
-//
-//		// 새 값 삽입
-//		data[i + 1] = value;
-//		priority[i + 1] = prio;
-//		count++;
-//	}
-//
-//	// 가장 높은 우선순위 값을 꺼냄
-//	int pop() {
-//		if (isEmpty()) {
-//			cout << "Queue is Empty" << endl;
-//			return -1;
-//		}
-//
-//		int result = data[0];
-//
-//		// 하나씩 앞으로 당기기
-//		for (int i = 1; i < count; i++) {
-//			data[i - 1] = data[i];
-//			priority[i - 1] = priority[i];
-//		}
-//
-//		count--;
-//		return result;
-//	}
-//
-//	// 디버깅용: 큐 안에 뭐 들었는지 보기
-//	void print() {
-//		cout << "Queue: ";
-//		for (int i = 0; i < count; i++) {
-//			cout << "[" << data[i] << ", p=" << priority[i] << "] ";
-//		}
-//		cout << endl;
-//	}
-//};
 
-
-// 내일 해시테이블 예정 
 #pragma region MyRegion
 //template <typename K, typename V>
 //class HashTable
@@ -321,28 +219,17 @@ public:
 //};
 #pragma endregion
 
-
 int main()
 {
-	PriorityQueue<int> pq;
+	HashTable<float, int> ht;
+	HashTable<int, int> ht2;
+	HashTable<const char*, int> ht3;
 
-	pq.push(5);
-	pq.push(3);
-	pq.push(6);
-	pq.push(7);
-	pq.push(1);
-	pq.push(9);
-
-	cout << pq.top() << endl;
-	pq.pop();
-	cout << pq.top() << endl;
-	pq.pop();
-	cout << pq.top() << endl;
-	cout << pq.isEmpty() << endl;
-
+	//cout << ht.hash_function(3.141592);
+	//cout << ht2.hash_function(23);
+	//cout << ht3.hash_function("Dinos");
+	//ht.insert(2, 3);
+	ht3.insert("I want to go home", 2);
 
 	return 0;
 }
-
-
-
